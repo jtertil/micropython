2e598e8660a0b4f62813936893e710016da197b1
jtertil <jtertil@gmail.com>
2019-07-13 18:54:50 +0200

    add button

--- chapter3/main.py
+++ chapter3/main.py
@@ -1,11 +1,19 @@
 import machine
 import time
 
-led = machine.Pin(2, machine.Pin.OUT)
+LED_PIN = 2  # D4
+BUTTON_PIN = 14  # D5
 
-while True:
-    led.off()
-    time.sleep(1)
+def blink():
+
+    led = machine.Pin(LED_PIN, machine.Pin.OUT)
+    button = machine.Pin(BUTTON_PIN, machine.Pin.IN, machine.Pin.PULL_UP)
+
+    while button.value():    
+        led.off()
+        time.sleep(0.2)
+        led.on()
+        time.sleep(0.2)
     led.on()
-    time.sleep(0.5)
 
+blink()
