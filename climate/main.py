a1e8e024081cf29a8c8c0e789e456317f09ea622
jtertil <jtertil@gmail.com>
2019-11-25 18:23:17 +0100

    setup, reconnect if connection lost

--- climate/main.py
+++ climate/main.py
@@ -4,22 +4,12 @@ import dht
 
 import urequests
 import time
+import setup
 
 # config
-LED_PIN = 2  # D4
-led = machine.Pin(LED_PIN, machine.Pin.OUT)
+led = machine.Pin(setup.LED_PIN, machine.Pin.OUT)
+sensor = dht.DHT22(machine.Pin(setup.DHT22_PIN))
 
-DHT22_PIN = 4  # D2
-sensor = dht.DHT22(machine.Pin(DHT22_PIN))
-
-WLAN_SSID = ''
-WLAN_PASSW = ''
-
-API_KEY = ''
-API_URL = 'https://api.thingspeak.com/update?api_key=' \
-          '{API_KEY}&' \
-          'field1={temperature}&' \
-          'field2={humidity}'
 
 
 def wifi_connect():
@@ -27,13 +17,14 @@ def wifi_connect():
     ap.active(False)
     wifi = network.WLAN(network.STA_IF)
 
-    if not wifi.isconnected():
+    while not wifi.isconnected():
+        print('Connecting to {}'.format(setup.WLAN_SSID))
         wifi.active(True)
-        wifi.connect(WLAN_SSID, WLAN_PASSW)
+        wifi.connect(setup.WLAN_SSID, setup.WLAN_PASSW)
+        time.sleep(5)
 
-        while not wifi.isconnected():
-            time.sleep(5)
-        print(wifi.ifconfig())
+    print('Connected to {}'.format(setup.WLAN_SSID))
+    print('Network config: {}'.format(wifi.ifconfig()))
 
 
 def read():
@@ -44,21 +35,23 @@ def read():
 
 
 def log_data(temperature, humidity):
-    url = API_URL.format(
-        API_KEY=API_KEY,
+    url = setup.API_URL.format(
+        API_KEY=setup.API_KEY,
         temperature=temperature,
         humidity=humidity)
-
-    response = urequests.get(url)
-    print(response.status_code)
+    try:
+        response = urequests.get(url)
+        print('Response code: ', response.status_code)
+    except Exception as err:
+        print('Request error: ', err)
 
 
 def run():
     while True:
         wifi_connect()
-        t,h = read()
-        log_data(t,h)
-        time.sleep(300)
+        t, h = read()
+        log_data(t, h)
+        time.sleep(setup.READ_INTERV * 60)
 
 
 run()
---
+++ climate/setup.py
@@ -0,0 +1,15 @@
+# config
+LED_PIN = 2  # D4
+DHT22_PIN = 4  # D2
+
+
+WLAN_SSID = ''
+WLAN_PASSW = ''
+
+API_KEY = ''
+API_URL = 'https://api.thingspeak.com/update?api_key=' \
+          '{API_KEY}&' \
+          'field1={temperature}&' \
+          'field2={humidity}'
+
+READ_INTERV = 1  # read interval in minutes
